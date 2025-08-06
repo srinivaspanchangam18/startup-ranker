9f3747b1243b7fd489d5bd14c83ba13c5cd591e2
srinivaspanchangam18 <srinivaspanchangam18@gmail.com>
2025-08-06 15:13:18 +0530

    Update startup_ranker.py

--- startup_ranker.py
+++ startup_ranker.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import MinMaxScaler
 # Load the dataset
 @st.cache_data
 def load_data():
-    return pd.read_csv("normalized_per.csv")
+    return pd.read_csv("AIC_DATA-Copied.csv")
 
 # KPI columns and weights
 kpi_cols = ['turnover', 'total_funding', 'employees', 'dev_stage_score', 'rev_per_emp', 'gst_filed']
@@ -94,5 +94,6 @@ if st.button("Get Rank"):
 
     st.success(f"Performance Score: **{score:.4f}**")
     st.info(f"Ranked **#{rank} out of {total} startups**")
+
 
 
